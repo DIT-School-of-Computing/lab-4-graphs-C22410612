aa39565e59eac009a82bafd46d475979280e006f
C22410612 <C22410612@mytudublin.ie>
2024-02-15 10:49:28 +0000

    Added Text / Colour / Axis to the Rainfall Bar Chart

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -31,18 +31,22 @@ public class Arrays extends PApplet
 		size(500, 500);
 
 		String[] m1 = months;
-		months[0] = "XXX";
+		// months[0] = "XXX"; ?
+
 		print(m1[0]);
+
 		for(int i = 0; i < months.length; i ++)
 		{
 			println("Month: " + months[i] + "\t" + rainfall[i]);
 		}
-		for (String s : m1) {
+
+		for (String s : m1)
+		{
 			println(s);
 		}
 
 		int minIndex = 0;
-		for(int i= 0 ; i < rainfall.length ; i ++)
+		for(int i = 0 ; i < rainfall.length ; i ++)
 		{
 			if (rainfall[i] < rainfall[minIndex])
 			{
@@ -51,7 +55,7 @@ public class Arrays extends PApplet
 		}
 		
 		int maxIndex = 0;
-		for(int i= 0 ; i < rainfall.length ; i ++)
+		for(int i = 0 ; i < rainfall.length ; i ++)
 		{
 			if (rainfall[i] > rainfall[maxIndex])
 			{
@@ -84,12 +88,11 @@ public class Arrays extends PApplet
 
 	}
 
-	public void setup() {
+	public void setup()
+	{
 		colorMode(HSB);
 		background(0);
 		randomize();
-		
-		
 	}
 
 	
@@ -97,11 +100,26 @@ public class Arrays extends PApplet
 	{	
 
 		background(0);
+
+		// Axis
+		fill(0,100,0);
+		stroke(255);
+		rect(50, 450, 1000, 3);
+		rect(50, 0, 3, 450);
+
+		// Bars
 		float w = width / (float)months.length;
 		for(int i = 0 ; i < months.length ;  i ++)
 		{
-			float x = map1(i, 0, months.length, 0, width);
-			rect(x, height, w, -rainfall[i]);
+			stroke(0);
+			fill(i * 25, 100, 100);
+			float x = map1(i, 0, months.length, 54, width);
+			float y = map1(i, 0, months.length, height - 50, 0);
+			rect(x, 449, w, -rainfall[i]);
+			
+			// Axis
+			text(months[i], x, 480);
+			text((40 * i), 20, y);
 		}
 	}
 }
